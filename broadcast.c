fc97e8d465d85e9259affe5521e765ccc07a9f5f
Ben Baker <bakerb845@gmail.com>
2016-10-07 14:19:23 -0700

    broadcasting catalog

--- broadcast.c
+++ broadcast.c
@@ -8,6 +8,8 @@
  *
  * @author Ben Baker
  *
+ * @copyright Apache 2
+ *
  */
 void broadcast_stations(MPI_Comm comm, const int root,
                         struct mceik_stations_struct *stations)
@@ -76,3 +78,66 @@ void broadcast_stations(MPI_Comm comm, const int root,
     }
     return;
 } 
+//============================================================================//
+/*!
+ * @brief Distributes the catalog to all processes on communicator from
+ *        root process
+ *
+ * @param[in] comm         MPI communicator
+ * @param[in] root         root process ID on communicator with catalog
+ *
+ * @param[in,out] catalog  on input this is the catalog on the root process.
+ *                         on output this is the catalog on all the processes
+ *                         on the communicator.
+ *
+ * @author Ben Baker
+ *
+ * @copyright Apache 2
+ *
+ */
+void broadcast_catalog(MPI_Comm comm, const int root,
+                       struct mceik_catalog_struct *catalog)
+{
+    int myid, nevents, nwork;
+    //------------------------------------------------------------------------//
+    //
+    // Get communicator info
+    MPI_Comm_rank(comm, &myid);
+    // Sizes
+    MPI_Bcast(&catalog->nevents, 1, MPI_INT, root, comm);
+    nevents = catalog->nevents;
+    if (nevents < 1){return;}
+    if (myid == root){nwork = catalog->obsPtr[nevents];}
+    MPI_Bcast(&nwork, 1, MPI_INT, root, comm);
+    // Integers
+    if (myid != root)
+    {
+        catalog->luseObs  = (int *) calloc((size_t) nwork,     sizeof(int));
+        catalog->pickType = (int *) calloc((size_t) nwork,     sizeof(int));
+        catalog->statPtr  = (int *) calloc((size_t) nwork,     sizeof(int));
+        catalog->obsPtr   = (int *) calloc((size_t) nevents+1, sizeof(int));
+    }
+    MPI_Bcast(catalog->luseObs,  nwork,     MPI_INT, root, comm);
+    MPI_Bcast(catalog->pickType, nwork,     MPI_INT, root, comm);
+    MPI_Bcast(catalog->statPtr,  nwork,     MPI_INT, root, comm);
+    MPI_Bcast(catalog->obsPtr,   nevents+1, MPI_INT, root, comm);
+    // Doubles
+    if (myid != root)
+    {
+        catalog->xsrc = (double *) calloc((size_t) nevents, sizeof(double));
+        catalog->ysrc = (double *) calloc((size_t) nevents, sizeof(double));
+        catalog->zsrc = (double *) calloc((size_t) nevents, sizeof(double));
+        catalog->t0   = (double *) calloc((size_t) nevents, sizeof(double));
+        catalog->tobs = (double *) calloc((size_t) nwork,   sizeof(double));
+        catalog->test = (double *) calloc((size_t) nwork,   sizeof(double));
+        catalog->var  = (double *) calloc((size_t) nwork,   sizeof(double));
+    }
+    MPI_Bcast(catalog->xsrc, nevents, MPI_DOUBLE, root, comm);
+    MPI_Bcast(catalog->ysrc, nevents, MPI_DOUBLE, root, comm);
+    MPI_Bcast(catalog->zsrc, nevents, MPI_DOUBLE, root, comm);
+    MPI_Bcast(catalog->t0,   nevents, MPI_DOUBLE, root, comm);
+    MPI_Bcast(catalog->tobs, nwork,   MPI_DOUBLE, root, comm);
+    MPI_Bcast(catalog->test, nwork,   MPI_DOUBLE, root, comm);
+    MPI_Bcast(catalog->var,  nwork,   MPI_DOUBLE, root, comm);
+    return;
+}
